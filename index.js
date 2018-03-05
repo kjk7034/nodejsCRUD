76515621f8241dd9a83b7007afd3c88f35072325
wise7034 <wise7034@wise7034ui-MacBook-Pro.local>
2018-03-05 13:34:24 +0900

    update sample

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const app = express()
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require("body-parser")
+const dbPath = path.join(__dirname, 'database.json')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+    console.log("Get Root")
+    response.end()
+})
+
+// CREATE
+app.post("/wise", (request, response) => {
+    fs.readFile(dbPath, { encoding: 'utf-8' }, (err, data) => {
+        if (!err) {
+            const database = JSON.parse(data)
+            database.datas.push(JSON.stringify(request.body))
+            fs.writeFile(dbPath, JSON.stringify(database), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+            response.end();
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+// READ
+app.get("/wise", (request, response) => {
+    fs.readFile(dbPath, { encoding: 'utf-8' }, (err, data) => {
+        if (!err) {
+            const database = JSON.parse(data)
+            console.log(database.datas[request.query.idx]);
+            response.end();
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+// PUT
+app.put("/wise", (request, response) => {
+    fs.readFile(dbPath, { encoding: 'utf-8' }, (err, data) => {
+        if (!err) {
+            const database = JSON.parse(data)
+            database.datas[request.query.idx] = JSON.stringify(request.body)
+            fs.writeFile(dbPath, JSON.stringify(database), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+            response.end();
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+// DELETE
+app.delete("/wise", (request, response) => {
+    fs.readFile(dbPath, { encoding: 'utf-8' }, (err, data) => {
+        if (!err) {
+            const database = JSON.parse(data)
+            database.datas[request.query.idx] = {}
+            console.log("database", database)
+            // fs.writeFile(dbPath, JSON.stringify(database), (err) => {
+            //     if (err) throw err;
+            //     console.log('The file has been saved!');
+            // });
+            response.end();
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+app.listen(3000, () => {
+    console.log("시작됨")
+})
